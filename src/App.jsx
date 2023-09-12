e1e785cfa13d1aac4d70f727b9a607b8d914041a
redoqsayan <sayan.mondol@redoq.com>
2023-09-12 17:37:49 +0530

    og test

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { Helmet } from "react-helmet-async";
+
+function App() {
+  const [data, setdata] = useState();
+
+  const fetchData = async () => {
+    const body = {
+      inputData: {
+        storeslug: "rose-fast-food",
+        product_Id: "6183775",
+      },
+    };
+
+    console.log(body, "body");
+    const response = await axios.post(
+      "https://webservice.dineapi.com/api/dine/storedata",
+      body
+    );
+
+    setdata(response?.data?.store);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <div
+        style={{
+          paddingTop: "20px",
+        }}
+      >
+        <div
+          style={{
+            fontWeight: "800",
+          }}
+        >
+          Store details
+        </div>
+        <div>{data?.store_Name}</div>
+      </div>
+      <Helmet>
+        <meta property="og:title" content={data?.store_name} />
+        <meta property="og:description" content={data?.store_Banner} />
+        <meta property="og:image" content={data?.store_Banner_Image} />
+        <meta property="twitter:title" content={data?.store_name} />
+        <meta property="twitter:description" content={data?.store_Banner} />
+        <meta property="twitter:image" content={data?.store_Banner_Image} />
+      </Helmet>
+    </div>
+  );
+}
+
+export default App;
